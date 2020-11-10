183c0b7c003d69ddca9c09dca9ac0727f6630259
Roman2281337 <55990897+Roman2281337@users.noreply.github.com>
2020-11-10 10:57:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-"""import vk_api
+import vk_api
 from datetime import datetime
 import time
 import requests
@@ -8,29 +8,38 @@ vk = vk_api.VkApi(token=token)
 vk._auth_token()
 #НА HEROKU на 3 часа меньше
 print('Бот успешно активирован!')
+print(datetime.now())
 
 
 
-
-legitID = []
+array = []
 
 def Bot():
+    print("Идет поиск уроков...")
+    now = datetime.now()
+    day_ = int(now.strftime("%d"))
     k = 192684000
     for i in range(999):
         try:
             r = requests.get("https://dnevnik.mos.ru/vcs/links?scheduled_lesson_id=" + str(k)).json()
-            legitID.append(str(r['_embedded']['link_views'][0]['scheduled_lesson_id']))
-            print(str(r['_embedded']['link_views'][0]['scheduled_lesson_id']),end=",")
+            s = r['_embedded']['link_views'][0]['start_date_time']
+            d1 = datetime.strptime(s, "%Y-%m-%dT%H:%M:%S+03:00")
+            day = int(d1.strftime("%d"))
+
+            if day >= day_:
+                array.append(r)
+                #print(str(r['_embedded']['link_views'][0]['scheduled_lesson_id']),end=",")
             k = k + 1
         except Exception:
             k=k+1
-
+    print("Поиск уроков закончен\n")
 
 
 
 
 def arbus():
-    #print(".",end="")
+    print(".",end="")
+    time.sleep(1)
     #str
     now = datetime.now()
     month_ = now.strftime("%m")
@@ -40,20 +49,15 @@ def arbus():
 
     if hour_ == "05" and int(minute_) == 25:
         print("start")
-        for i in legitID:
-            r = requests.get("https://dnevnik.mos.ru/vcs/links?scheduled_lesson_id=" + str(i)).json()
-            s = r['_embedded']['link_views'][0]['start_date_time']
+        for i in range(len(array)):
+            s = array[i]['_embedded']['link_views'][0]['start_date_time']
             d1 = datetime.strptime(s, "%Y-%m-%dT%H:%M:%S+03:00")
             month = d1.strftime("%m")
             day = d1.strftime("%d")
             hour = d1.strftime("%H")
             minute = d1.strftime("%M")
             if hour == "05" and int(minute)-5 == int(minute_) and int(day_) == int(day):
-                text = (f"\nДата: {month}-{day}\nВремя начала урока: {hour}:{minute} " + "\nURL 1: " +
-                        r['_embedded']['link_views'][0][
-                            'link_url'] + "\nURL 2: https://dnevnik.mos.ru/conference/?scheduled_lesson_id=" + str(
-                            r['_embedded']['link_views'][0]['scheduled_lesson_id']))
-
+                text = (f"\nДата урока: {day}-{month}-2020\nВремя начала урока: {hour}:{minute} " + "\nСсылка: " +array[i]['_embedded']['link_views'][0]['link_url'])
                 vk.method("messages.send", {"peer_id": 2000000002, "message": text, "random_id": 0})
                 print("Сообщение отправлено!")
                 time.sleep(60)
@@ -65,20 +69,16 @@ def arbus():
 
     if hour_ == "06" and int(minute_) == 25:
         print("start")
-        for i in legitID:
-            r = requests.get("https://dnevnik.mos.ru/vcs/links?scheduled_lesson_id=" + str(i)).json()
-            s = r['_embedded']['link_views'][0]['start_date_time']
+        for i in range(len(array)):
+            s = array[i]['_embedded']['link_views'][0]['start_date_time']
             d1 = datetime.strptime(s, "%Y-%m-%dT%H:%M:%S+03:00")
             month = d1.strftime("%m")
             day = d1.strftime("%d")
             hour = d1.strftime("%H")
             minute = d1.strftime("%M")
-            if hour == "06" and int(minute)-5 == int(minute_) and int(day_) == int(day):
-                text = (f"\nДата: {month}-{day}\nВремя начала урока: {hour}:{minute} " + "\nURL 1: " +
-                        r['_embedded']['link_views'][0][
-                            'link_url'] + "\nURL 2: https://dnevnik.mos.ru/conference/?scheduled_lesson_id=" + str(
-                            r['_embedded']['link_views'][0]['scheduled_lesson_id']))
-
+            if hour == "06" and int(minute) - 5 == int(minute_) and int(day_) == int(day):
+                text = (f"\nДата урока: {day}-{month}-2020\nВремя начала урока: {hour}:{minute} " + "\nСсылка: " +
+                        array[i]['_embedded']['link_views'][0]['link_url'])
                 vk.method("messages.send", {"peer_id": 2000000002, "message": text, "random_id": 0})
                 print("Сообщение отправлено!")
                 time.sleep(60)
@@ -88,20 +88,18 @@ def arbus():
 
     if hour_ == "07" and int(minute_) == 25:
         print("start")
-        for i in legitID:
-            r = requests.get("https://dnevnik.mos.ru/vcs/links?scheduled_lesson_id=" + str(i)).json()
-            s = r['_embedded']['link_views'][0]['start_date_time']
+        for i in range(len(array)):
+            s = array[i]['_embedded']['link_views'][0]['start_date_time']
             d1 = datetime.strptime(s, "%Y-%m-%dT%H:%M:%S+03:00")
             month = d1.strftime("%m")
             day = d1.strftime("%d")
             hour = d1.strftime("%H")
             minute = d1.strftime("%M")
-            if hour == "07" and int(minute)-5 == int(minute_) and int(day_) == int(day):
-                text = (f"\nДата: {month}-{day}\nВремя начала урока: {hour}:{minute} " + "\nURL 1: " +
-                        r['_embedded']['link_views'][0][
-                            'link_url'] + "\nURL 2: https://dnevnik.mos.ru/conference/?scheduled_lesson_id=" + str(
-                            r['_embedded']['link_views'][0]['scheduled_lesson_id']))
-
+            if hour == "07" and int(minute) - 5 == int(minute_) and int(day_) == int(day):
+                text = (f"\nДата урока: {day}-{month}-2020\nВремя начала урока: {hour}:{minute} " + "\nСсылка: " +
+                        array[i]['_embedded']['link_views'][0]['link_url'])
+                print(text)
+                print("Идет отправка сообщения...")
                 vk.method("messages.send", {"peer_id": 2000000002, "message": text, "random_id": 0})
                 print("Сообщение отправлено!")
                 time.sleep(60)
@@ -111,20 +109,18 @@ def arbus():
 
     if hour_ == "08" and int(minute_) == 20:
         print("start")
-        for i in legitID:
-            r = requests.get("https://dnevnik.mos.ru/vcs/links?scheduled_lesson_id=" + str(i)).json()
-            s = r['_embedded']['link_views'][0]['start_date_time']
+        for i in range(len(array)):
+            s = array[i]['_embedded']['link_views'][0]['start_date_time']
             d1 = datetime.strptime(s, "%Y-%m-%dT%H:%M:%S+03:00")
             month = d1.strftime("%m")
             day = d1.strftime("%d")
             hour = d1.strftime("%H")
             minute = d1.strftime("%M")
-            if hour == "08" and int(minute)-5 == int(minute_) and int(day_) == int(day):
-                text = (f"\nДата: {month}-{day}\nВремя начала урока: {hour}:{minute} " + "\nURL 1: " +
-                        r['_embedded']['link_views'][0][
-                            'link_url'] + "\nURL 2: https://dnevnik.mos.ru/conference/?scheduled_lesson_id=" + str(
-                            r['_embedded']['link_views'][0]['scheduled_lesson_id']))
-
+            if hour == "08" and int(minute) - 5 == int(minute_) and int(day_) == int(day):
+                text = (f"\nДата урока: {day}-{month}-2020\nВремя начала урока: {hour}:{minute} " + "\nСсылка: " +
+                        array[i]['_embedded']['link_views'][0]['link_url'])
+                print(text)
+                print("Идет отправка сообщения...")
                 vk.method("messages.send", {"peer_id": 2000000002, "message": text, "random_id": 0})
                 print("Сообщение отправлено!")
                 time.sleep(60)
@@ -134,20 +130,18 @@ def arbus():
 
     if hour_ == "09" and int(minute_) == 25:
         print("start")
-        for i in legitID:
-            r = requests.get("https://dnevnik.mos.ru/vcs/links?scheduled_lesson_id=" + str(i)).json()
-            s = r['_embedded']['link_views'][0]['start_date_time']
+        for i in range(len(array)):
+            s = array[i]['_embedded']['link_views'][0]['start_date_time']
             d1 = datetime.strptime(s, "%Y-%m-%dT%H:%M:%S+03:00")
             month = d1.strftime("%m")
             day = d1.strftime("%d")
             hour = d1.strftime("%H")
             minute = d1.strftime("%M")
-            if hour == "09" and int(minute)-5 == int(minute_) and int(day_) == int(day):
-                text = (f"\nДата: {month}-{day}\nВремя начала урока: {hour}:{minute} " + "\nURL 1: " +
-                        r['_embedded']['link_views'][0][
-                            'link_url'] + "\nURL 2: https://dnevnik.mos.ru/conference/?scheduled_lesson_id=" + str(
-                            r['_embedded']['link_views'][0]['scheduled_lesson_id']))
-
+            if hour == "09" and int(minute) - 5 == int(minute_) and int(day_) == int(day):
+                text = (f"\nДата урока: {day}-{month}-2020\nВремя начала урока: {hour}:{minute} " + "\nСсылка: " +
+                        array[i]['_embedded']['link_views'][0]['link_url'])
+                print(text)
+                print("Идет отправка сообщения...")
                 vk.method("messages.send", {"peer_id": 2000000002, "message": text, "random_id": 0})
                 print("Сообщение отправлено!")
                 time.sleep(60)
@@ -156,20 +150,18 @@ def arbus():
 
     if hour_ == "10" and int(minute_) == 25:
         print("start")
-        for i in legitID:
-            r = requests.get("https://dnevnik.mos.ru/vcs/links?scheduled_lesson_id=" + str(i)).json()
-            s = r['_embedded']['link_views'][0]['start_date_time']
+        for i in range(len(array)):
+            s = array[i]['_embedded']['link_views'][0]['start_date_time']
             d1 = datetime.strptime(s, "%Y-%m-%dT%H:%M:%S+03:00")
             month = d1.strftime("%m")
             day = d1.strftime("%d")
             hour = d1.strftime("%H")
             minute = d1.strftime("%M")
-            if hour == "10" and int(minute)-5 == int(minute_) and int(day_) == int(day):
-                text = (f"\nДата: {month}-{day}\nВремя начала урока: {hour}:{minute} " + "\nURL 1: " +
-                        r['_embedded']['link_views'][0][
-                            'link_url'] + "\nURL 2: https://dnevnik.mos.ru/conference/?scheduled_lesson_id=" + str(
-                            r['_embedded']['link_views'][0]['scheduled_lesson_id']))
-
+            if hour == "10" and int(minute) - 5 == int(minute_) and int(day_) == int(day):
+                text = (f"\nДата урока: {day}-{month}-2020\nВремя начала урока: {hour}:{minute} " + "\nСсылка: " +
+                        array[i]['_embedded']['link_views'][0]['link_url'])
+                print(text)
+                print("Идет отправка сообщения...")
                 vk.method("messages.send", {"peer_id": 2000000002, "message": text, "random_id": 0})
                 print("Сообщение отправлено!")
                 time.sleep(60)
@@ -179,20 +171,18 @@ def arbus():
 
     if hour_ == "11" and int(minute_) == 25:
         print("start")
-        for i in legitID:
-            r = requests.get("https://dnevnik.mos.ru/vcs/links?scheduled_lesson_id=" + str(i)).json()
-            s = r['_embedded']['link_views'][0]['start_date_time']
+        for i in range(len(array)):
+            s = array[i]['_embedded']['link_views'][0]['start_date_time']
             d1 = datetime.strptime(s, "%Y-%m-%dT%H:%M:%S+03:00")
             month = d1.strftime("%m")
             day = d1.strftime("%d")
             hour = d1.strftime("%H")
             minute = d1.strftime("%M")
-            if hour == "11" and int(minute)-5 == int(minute_) and int(day_) == int(day):
-                text = (f"\nДата: {month}-{day}\nВремя начала урока: {hour}:{minute} " + "\nURL 1: " +
-                        r['_embedded']['link_views'][0][
-                            'link_url'] + "\nURL 2: https://dnevnik.mos.ru/conference/?scheduled_lesson_id=" + str(
-                            r['_embedded']['link_views'][0]['scheduled_lesson_id']))
-
+            if hour == "11" and int(minute) - 5 == int(minute_) and int(day_) == int(day):
+                text = (f"\nДата урока: {day}-{month}-2020\nВремя начала урока: {hour}:{minute} " + "\nСсылка: " +
+                        array[i]['_embedded']['link_views'][0]['link_url'])
+                print(text)
+                print("Идет отправка сообщения...")
                 vk.method("messages.send", {"peer_id": 2000000002, "message": text, "random_id": 0})
                 print("Сообщение отправлено!")
                 time.sleep(60)
@@ -204,18 +194,18 @@ def arbus():
 
     if hour_ == "12" and int(minute_) == 20:
         print("start")
-        for i in legitID:
-            r = requests.get("https://dnevnik.mos.ru/vcs/links?scheduled_lesson_id=" + str(i)).json()
-            s = r['_embedded']['link_views'][0]['start_date_time']
+        for i in range(len(array)):
+            s = array[i]['_embedded']['link_views'][0]['start_date_time']
             d1 = datetime.strptime(s, "%Y-%m-%dT%H:%M:%S+03:00")
             month = d1.strftime("%m")
             day = d1.strftime("%d")
             hour = d1.strftime("%H")
             minute = d1.strftime("%M")
-            if hour == "12" and int(minute)-5 == int(minute_) and int(day_) == int(day):
-                text = (f"\nДата: {month}-{day}\nВремя начала урока: {hour}:{minute} " + "\nURL 1: " +
-                        r['_embedded']['link_views'][0]['link_url'] + "\nURL 2: https://dnevnik.mos.ru/conference/?scheduled_lesson_id="+str(r['_embedded']['link_views'][0]['scheduled_lesson_id']))
-
+            if hour == "12" and int(minute) - 5 == int(minute_) and int(day_) == int(day):
+                text = (f"\nДата урока: {day}-{month}-2020\nВремя начала урока: {hour}:{minute} " + "\nСсылка: " +
+                        array[i]['_embedded']['link_views'][0]['link_url'])
+                print(text)
+                print("Идет отправка сообщения...")
                 vk.method("messages.send", {"peer_id": 2000000002, "message": text, "random_id": 0})
                 print("Сообщение отправлено!")
                 time.sleep(60)
@@ -223,6 +213,5 @@ def arbus():
 
 
 Bot()
-print(legitID)
 while True:
-    arbus()"""
+    arbus()
