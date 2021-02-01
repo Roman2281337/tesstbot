4955fe0a556517cdc187e0715b559595654d759b
Roman2281337 <55990897+Roman2281337@users.noreply.github.com>
2021-02-01 13:27:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from datetime import datetime
-
 import vk_api
 import time
-import tk
 
-token = tk.token
+token = "8510e0e7112f7b179bf58b82b7f540825ab95a471ae66ce512613e12a09843d9ed41a27ac1ce857947b80"
+
 vk = vk_api.VkApi(token=token)
 vk._auth_token()
 
