48743a7e3ef9c36028f544bf8609e2b24c17e1a5
Roman2281337 <55990897+Roman2281337@users.noreply.github.com>
2021-02-01 19:43:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -32,21 +32,26 @@ old_hour = hour
 old_minute = minute
 
 while True:
-    time.sleep(1)
-    now = datetime.now() + timedelta(hours=3)
-    years = now.strftime("%Y")
-    month = now.strftime("%m")
-    day = now.strftime("%d")
-    hour = now.strftime("%H")
-    minute = now.strftime("%M")
-    second = now.strftime("%S")
+    try:
+        time.sleep(1)
+        now = datetime.now() + timedelta(hours=3)
+        years = now.strftime("%Y")
+        month = now.strftime("%m")
+        day = now.strftime("%d")
+        hour = now.strftime("%H")
+        minute = now.strftime("%M")
+        second = now.strftime("%S")
 
-    if old_hour != hour or old_minute != minute:
-        like = vk.method("likes.getList",{"type": "photo", "owner_id": owner_id, "item_id": "457252443", "random_id": 0})
-        count_like = like['count']
-        vk.method("status.set", {"text": f"{day}.{month}.{years} | {hour}:{minute} | На аве {count_like}♥", "random_id": 0})
-        print("статус изменен")
-        old_hour = hour
-        old_minute = minute
+        if old_hour != hour or old_minute != minute:
+            like = vk.method("likes.getList",{"type": "photo", "owner_id": owner_id, "item_id": "457252443", "random_id": 0})
+            count_like = like['count']
+            vk.method("status.set", {"text": f"{day}.{month}.{years} | {hour}:{minute} | На аве {count_like}♥", "random_id": 0})
+            print("статус изменен")
+            old_hour = hour
+            old_minute = minute
+    except Exception as e:
+        pass
+    print(e)
+
 
 
